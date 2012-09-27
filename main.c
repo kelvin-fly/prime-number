e06b7aaf5d1de073540a9b9c52090b29b7d2e9f4
kelvin.fly <kelvin.fly.lc@gmail.com>
2012-09-27 02:00:52 -0700

    get prime number and calculate the sum of units

--- main.c
+++ main.c
@@ -2,13 +2,14 @@
 #include<math.h>
 
 #define NUM 100
+int sum(int *arr,int size);  //Translation of the array!!!
 
 int main()
 {
 	int array[NUM]={2,3,5},flag=0,i,j,dnumber=3;
-	for(i=3;i<NUM;i++)
+	for(i=6;i<NUM;i++)
 	{
-		for(j=2;j<(i/2);j++)
+		for(j=2;j<(i/2);j++)  //cause 5/2==2,so start with 6!!!
 		{
 			if(i%j==0)
 			{
@@ -16,7 +17,7 @@ int main()
 				break;
 			}
 			else
-				flag=1;
+				flag=1;   //flag is frequently erasuring!!!
 		}
 		if(flag==1)
 		{
@@ -32,5 +33,17 @@ int main()
 			printf("\n");
 	}
 	printf("\n");
+
+	sum(array,dnumber);
 	return 0;
+}
+
+int sum(int *arr, int size)
+{
+	int i, sum=0;
+	for(i=0; i<size; i++)
+	{
+		sum+=(arr[i]%10);
+	}
+		printf("Units of prime number's sum:%d\n",sum);
 }
